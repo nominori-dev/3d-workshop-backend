5e66c6b28e250f38fe34bca0ba7471887d531085
nominori-dev <antonshimchico37@gmail.com>
2023-07-03 23:40:47 +0200

    project initialization

---
+++ src/test/java/com/nominori/oms/OmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nominori.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
