58b6913510eff375c47c73e22d534c10663dbc65
nominori-dev <antonshimchico37@gmail.com>
2023-07-16 17:49:01 +0200

    docs(swagger): add springdoc dependency & configuration

---
+++ src/main/java/com/nominori/oms/core/OpenAPIConfiguration.java
@@ -0,0 +1,24 @@
+package com.nominori.oms.core;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "3D Workshop API",
+                description = "The 3D Assets Workshop REST API powers an online shop for managing and selling 3D assets effortlessly.",
+                contact = @Contact(
+                        name = "nominori-dev",
+                        url = "https://github.com/nominori-dev",
+                        email = "nominori@bdv.pw"
+                ),
+                license = @License(
+                        name = "MIT License",
+                        url = "https://github.com/nominori-dev/3d-workshop-backend/LICENSE"
+                )
+        )
+)
+public class OpenAPIConfiguration {
+}
