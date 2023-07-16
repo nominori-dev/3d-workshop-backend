0ce644a0a0ae554c0999e1e582e32bb33492b1a9
nominori-dev <antonshimchico37@gmail.com>
2023-07-16 18:46:54 +0200

    feat!: added RabbitMQ messaging support along with dev-only message demo

---
+++ src/main/java/com/nominori/oms/core/message/DemoMessage.java
@@ -0,0 +1,14 @@
+package com.nominori.oms.core.message;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DemoMessage {
+    private String content;
+}
---
+++ src/main/java/com/nominori/oms/core/message/MessagingDemo.java
@@ -0,0 +1,27 @@
+package com.nominori.oms.core.message;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Profile("dev")
+@Component
+@Slf4j
+public class MessagingDemo {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public MessagingDemo(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.rabbitTemplate.convertAndSend("q.demo", new DemoMessage("Demo Message Object"));
+    }
+
+    @RabbitListener(queues = "q.demo")
+    public void onDemo(DemoMessage event){
+        log.info("Demo request received: {}", event.getContent());
+    }
+
+}
