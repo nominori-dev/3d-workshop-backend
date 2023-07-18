a0d5453ddd7b6568b97296051f5a760f072a5a34
nominori-dev <antonshimchico37@gmail.com>
2023-07-18 20:01:19 +0200

    refactor(schema)!: changed customer id from serial to uuid

--- src/main/resources/db/changelog/migrations/create-customer-schema.sql
+++ src/main/resources/db/changelog/migrations/create-customer-schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE oms_customers (
-    id serial PRIMARY KEY,
+    id uuid PRIMARY KEY,
     name VARCHAR (50) NOT NULL,
     email VARCHAR (255) UNIQUE NOT NULL,
     created_on TIMESTAMP NOT NULL
