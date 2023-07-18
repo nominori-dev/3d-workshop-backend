f11c85b264a5932328cea412d68b86f62ef26d97
nominori-dev <antonshimchico37@gmail.com>
2023-07-18 20:13:30 +0200

    refactor(schema): customer id references to user id and changed init order

--- src/main/resources/db/changelog/migrations/create-customer-schema.sql
+++ src/main/resources/db/changelog/migrations/create-customer-schema.sql
@@ -1,6 +1,9 @@
 CREATE TABLE oms_customers (
-    id uuid PRIMARY KEY,
+    id uuid,
     name VARCHAR (50) NOT NULL,
     email VARCHAR (255) UNIQUE NOT NULL,
-    created_on TIMESTAMP NOT NULL
+    created_on TIMESTAMP NOT NULL,
+    CONSTRAINT fk_user
+        FOREIGN KEY (id)
+              REFERENCES oms_users(id)
 )
