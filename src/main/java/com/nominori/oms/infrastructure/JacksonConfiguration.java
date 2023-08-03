05920afa618c0f9869ba09bcdf2f0ef782293492
nominori-dev <antonshimchico37@gmail.com>
2023-08-03 02:01:29 +0200

    feat: add JavaTimeModule from jsr310

---
+++ src/main/java/com/nominori/oms/infrastructure/JacksonConfiguration.java
@@ -0,0 +1,21 @@
+package com.nominori.oms.infrastructure;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    @Primary
+    public ObjectMapper objectMapper() {
+        return JsonMapper.builder()
+                .addModule(new JavaTimeModule())
+                .build();
+    }
+
+}
--- src/main/java/com/nominori/oms/infrastructure/message/RabbitMQConfig.java
+++ src/main/java/com/nominori/oms/infrastructure/message/RabbitMQConfig.java
@@ -1,5 +1,7 @@
 package com.nominori.oms.infrastructure.message;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
@@ -7,13 +9,16 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 @RequiredArgsConstructor
 public class RabbitMQConfig {
 
     private final CachingConnectionFactory cachingConnectionFactory;
+    private final ObjectMapper objectMapper;
 
     @Profile("dev")
     @Bean
@@ -23,7 +28,7 @@ public class RabbitMQConfig {
 
     @Bean
     public Jackson2JsonMessageConverter converter(){
-        return new Jackson2JsonMessageConverter();
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 
     @Bean
